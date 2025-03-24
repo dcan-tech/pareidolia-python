5db23fc47307f4d9d1b454819f20683ff4304f22
Dylan Canfield <smigs911@hotmail.com>
2025-03-24 11:42:22 -0700

    Add CLI support for width, height, and seed

--- pareidolia.py
+++ pareidolia.py
@@ -1,20 +1,35 @@
 import os
 import random
 from PIL import Image
+import argparse
+
+# CLI argument parser
+parser = argparse.ArgumentParser(description="Generate random static images.")
+parser.add_argument("--width", type=int, default=128, help="Width of image")
+parser.add_argument("--height", type=int, default=128, help="Height of image")
+parser.add_argument("--seed", type=int, default=None, help="Optional random seed")
+
+args = parser.parse_args()
+
+# use arguments
+width = args.width
+height = args.height
+seed = args.seed
+rng = random.Random(seed)
+
 
 
 # output directory exists?
 os.makedirs("output", exist_ok=True)
 
-# image size
-width, height = 128, 128
+
 
 # create a new RGB imagae
 image = Image.new("RGB", (width, height))
 
 for x in range(width):
     for y in range(height):
-        gray = random.randint(0, 255)
+        gray = rng.randint(0, 255)
         image.putpixel((x, y), (gray, gray, gray))
 
 image.save("output/image.png")
